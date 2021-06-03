b484802832fe69c5594ab19987533cdc0916642b
nadiacontreras-code <nadiabrenda.cs@gmail.com>
2021-06-03 08:38:27 -0500

    CheckBtn
    Agregando función del addEventListener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let checkBtn = document.querySelector('.checkBtn');
+
+checkBtn.addEventListener('click',()=>{
+    console.log("hiciste click");
+});
