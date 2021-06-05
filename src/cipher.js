aaa7f776441132a5a544c0e59a948ed5a1e44dd2
nadiacontreras-code <nadiabrenda.cs@gmail.com>
2021-06-05 17:13:36 -0500

    CSS cambios
    Practicando los cambios en css

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,32 @@
 const cipher = {
   // ...
-};
+}; 
 
 export default cipher;
+
+let checkBtn = document.getElementById('send');
+let mensaje1 = document.getElementById('mensaje1').value;
+let mensaje2 = document.getElementById('mensaje2').value;
+ 
+// Activando botón  y obteniendo contenido de box1 //
+
+
+let box1 = checkBtn.addEventListener('click', ()=> { 
+  //alert("Probando"); -probando funcionalidad del botón
+  console.log(document.getElementById('mensaje1').value);
+ // console.log(mensaje1); No me funciona la consola con esto
+  }); 
+  /*checkBtn.addEventListener('click', getMensaje1);
+  function getMensaje1 () {
+  console.log(document.getElementById('mensaje1').value);
+  };*/
+
+// Moviendo de box1 to box 2 //
+
+    /*if ( getMessageBox==="") {
+   /alert ("Ingresa tu mensaje");
+   }
+     console.log(mensaje1); 
+  
+    };   */ 
+
--- src/index.js
+++ src/index.js
@@ -2,8 +2,3 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-let checkBtn = document.querySelector('.checkBtn');
-
-checkBtn.addEventListener('click',()=>{
-    console.log("hiciste click");
-});
