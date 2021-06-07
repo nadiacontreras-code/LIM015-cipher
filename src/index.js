0a1502927c93950eb039ed7193e05dbfa1ee056c
nadiacontreras-code <nadiabrenda.cs@gmail.com>
2021-06-07 09:58:04 -0500

    Mover funciones de cipher a index
    Trabajar en index porque cipher es para cifrar y descifrar

--- src/index.js
+++ src/index.js
@@ -2,3 +2,31 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
+// Guardando contenido de box1 //
+function getMessage () {
+    let mensaje1 = document.getElementById('mensaje1').value;
+  }
+  // Creando la variable para el evento click//
+  let checkBtn = document.getElementById('send');
+  
+  // USando el EventListener //
+  
+   /* checkBtn.addEventListener('click', getBox1);
+    function getBox1 () {
+    console.log(document.getElementById('mensaje1').value);*/
+  
+  checkBtn.addEventListener('click', ()=> {
+    let mensaje1 = document.getElementById('mensaje1').value;
+    //alert("Probando"); -probando funcionalidad del botón
+    //console.log(document.getElementById('mensaje1').value);
+   console.log(mensaje1);
+    
+  // Moviendo de box1 to box 2 //
+  
+  // Creando un nuevo elemento y guardandolo en una variable//
+  //let box2 = document.createElement('textarea');
+  let texto2 = document.getElementById('mensaje2');
+  //Creando el texto //
+  texto2.textContent = mensaje1
+});
+  //texto2.appendChild(box2);
